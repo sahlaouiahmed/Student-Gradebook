1cbde84e0eca2138e0926569294006074871b0d1
sahlaouiahmed <sahlaouiahmad@outlook.com>
2024-08-29 18:27:02 +0000

    Define the Student class

--- run.py
+++ run.py
@@ -8,3 +8,26 @@ def authenticate_google_sheets(credentials_path, sheet_name):
     creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
     client = gspread.authorize(creds)
     return client.open(sheet_name).sheet1
+
+
+# Define the Student class
+class Student:
+    def __init__(self, name, grades):
+        self.name = name
+        self.grades = grades
+        self.average = sum(grades.values()) / len(grades)
+        self.status = 'Pass' if self.average >= 50 else 'Fail'
+        self.grade =  self.assign_grade() 
+
+    #Assign a grade to each student based on his average
+    def assign_grade(self):
+        if self.average >= 90:
+            return 'A'
+        elif self.average >= 80:
+            return 'B'
+        elif self.average >= 70:
+            return 'C'
+        elif self.average >= 60:
+            return 'D'
+        else:
+            return 'F'  
